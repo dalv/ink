c4c052d09cb0710c943a1aeda72ad633eae20311
Vlad Tamas <vlad.tamas@gmail.com>
2014-07-10 13:28:56 +0300

    switched database config to MongoLab

--- config/database.js
+++ config/database.js
@@ -1,3 +1,9 @@
 module.exports = {
-	url : 'mongodb://localhost:27017/ink'
+
+//  Local
+//	url : 'mongodb://localhost:27017/ink'
+
+// MongoLab Host
+url: 'mongodb://admin:nVf0IpHnX1PC@ds055699.mongolab.com:55699/ink'
+
 };	
