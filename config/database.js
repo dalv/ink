7f4a5344f6c4c1217a10c4fb61d832f6e0f31f50
Vlad Tamas <vlad.tamas@gmail.com>
2014-07-10 18:02:28 +0300

    formatting

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 module.exports = {
+	// Local
+	// url : 'mongodb://localhost:27017/ink'
 
-//  Local
-//	url : 'mongodb://localhost:27017/ink'
-
-// MongoLab Host
-url: 'mongodb://admin:nVf0IpHnX1PC@ds055699.mongolab.com:55699/ink'
-
+	// MongoLab Host
+	url: 'mongodb://admin:nVf0IpHnX1PC@ds055699.mongolab.com:55699/ink'
 };	
